ae51c97638849667ec9716c36793cfc3dd1468d8
Wooseong <thehousedone@gmail.com>
2025-06-26 11:40:54 +0900

    Initial Commit

---
+++ practice_UIKit_standard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_UIKit_standard
+//
+//  Created by 김우성 on 6/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
