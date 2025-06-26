cc36dac0901a77eafc7d745e5ecbf0a6586aa47a
Wooseong <thehousedone@gmail.com>
2025-06-26 16:26:40 +0900

    CollectionView 학습

--- practice_UIKit_standard/CollectionView/CustomCollectionViewController.swift
+++ practice_UIKit_standard/CollectionView/CustomCollectionViewController.swift
@@ -12,6 +12,8 @@ import UIKit
 private enum Const {
     static let cellWidth: CGFloat = 100.0
     static let cellHeight: CGFloat = 100.0
+//    static let cellWidth: CGFloat = UIScreen.main.bounds.width
+//    static let cellHeight: CGFloat = 600.0
     
     static let lineSpacing: CGFloat = 10.0
     static let interItemSpacing: CGFloat = 10.0
@@ -30,28 +32,36 @@ class CustomCollectionViewController: UIViewController {
     private lazy var collectionView: UICollectionView = {
         // layout을 반드시 선언해주어야 한다! 그리고 어떤 레이아웃을 쓸건지 넣어주어야 한다.
         let layout = UICollectionViewFlowLayout() // 제공된 Flow 레이아웃을 넣어준다. 만약 디자이너가 이런걸로 구현 불가능한 것을 원한다? 그럼 UICollectionViewLayout을 서브클래싱해서 각각의 셀들이 나타날 때 어떻게 배치할것인지를 커스텀해야 하겠다..
-        layout.scrollDirection = .vertical
+        layout.scrollDirection = .horizontal
         layout.itemSize = .init(width: Const.cellWidth, height: Const.cellHeight)
         // layout.estimatedItemSize // 예상치를 줌으로써 로딩 성능을 향상시키는 방법이 제공된다.
         layout.minimumLineSpacing = Const.lineSpacing // 버티컬 스크롤일때는 행간 간격을 의미한다.
         layout.minimumInteritemSpacing = Const.interItemSpacing // 버티컬 스크롤일때는 좌우 셀간의 간격을 의미한다.
+//        layout.minimumLineSpacing = 0.0 // 버티컬 스크롤일때는 행간 간격을 의미한다.
+//        layout.minimumInteritemSpacing = 0.0 // 버티컬 스크롤일때는 좌우 셀간의 간격을 의미한다.
         layout.sectionInset = .init( // 팁: control + m 누르면 바로 아래로 정렬됨!
             top: 0.0,
             left: Const.sectionInsetLeft,
+//            left: 0.0,
             bottom: 0.0,
             right: Const.sectionInsetRight
+//            right: 0.0
         )// 컬렉션 뷰가 갖고 있는 사이즈가 있을 때, 셀들이 들어가야 하는 크기에 대한 inset 값을 말한다.
         
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.backgroundColor = .blue.withAlphaComponent(0.2)
+//        collectionView.isPagingEnabled = true
         
         collectionView.dataSource = self
         collectionView.delegate = self
         
         // 컬렉션 뷰는 테이블 뷰와 달리 커스텀 셀을 직접 구현해주어야 한다!
         collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.id)
+        // 컬렉션 뷰는 헤더 또한 reuseable 풀에 두어 재사용하기 때문에 등록을 해 주어야 한다.
+        collectionView.register(CustomCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: CustomCollectionViewHeader.id)
+        // forSupplementaryViewOfKind 는 우리가 등록하려는 reusable 뷰가 어떤 종류인지를 명시하라고 요구한다.
         
         return collectionView
     }()
@@ -102,12 +112,31 @@ extension CustomCollectionViewController: UICollectionViewDataSource {
         
         return cell ?? .init() // cell이 nil일 경우 기본 UICollectionViewCell()로 대체. 이건 약간 임시 땜빵 스타일이고, cell 정의할 때에 guard 하고 else { fatalError() 처리하는 게 실전형.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: CustomCollectionViewHeader.id, for: indexPath) as? CustomCollectionViewHeader
+            
+            headerView?.configure(title: "헤더 \(indexPath.section)")
+            
+            return headerView ?? .init()
+        default:
+            return .init()
+        }
+    }
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
 // 사용자 인터렉션 뿐 아니라 UI 레이아웃을 동적으로 사용할때도 델리게이트 내부에서 아이템 사이즈 등 값을 전달할 수 있는데,
 // FlowLayout을 사용할 때는 반드시 그냥 CollectionViewDelegate가 아닌 CollectionViewDelegateFlowLayout을 채택해야 한다.
 extension CustomCollectionViewController: UICollectionViewDelegateFlowLayout {
-    // 필요한 경우 델리게이트 메서드 추가
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let width = collectionView.bounds.width
+        let height = 40.0
+        
+        return .init(width: width, height: height)
+    }
 }
 
--- practice_UIKit_standard/CollectionView/CustomCollectionViewHeader.swift
+++ practice_UIKit_standard/CollectionView/CustomCollectionViewHeader.swift
@@ -1,5 +1,5 @@
 //
-//  CustomCollectionViewCell.swift
+//  CustomCollectionViewHeader.swift
 //  practice_UIKit_standard
 //
 //  Created by 김우성 on 6/26/25.
@@ -7,16 +7,16 @@
 
 import UIKit
 
-class CustomCollectionViewCell: UICollectionViewCell {
+class CustomCollectionViewHeader: UICollectionReusableView { // ReusableView 라는 클래스를 상속받아야 한다.
     
     // MARK: - Identifier
-    static let id = "CustomCollectionViewCell"
+    static let id = "CustomCollectionViewHeader"
     
     // MARK: - UI Components
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = .black
-        label.font = .systemFont(ofSize: 18.0, weight: .bold)
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
@@ -32,11 +32,11 @@ class CustomCollectionViewCell: UICollectionViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    } // UIViewController, UIView, 혹은 NSCoding을 따르는 클래스에서 Storyboard나 XIB에서 초기화될 가능성을 차단하기 위한 패턴임
+    }
     
     // MARK: - Setup Methods
     private func setupUI() {
-        backgroundColor = .red.withAlphaComponent(0.3)
+        backgroundColor = .green.withAlphaComponent(0.5)
         addSubview(titleLabel)
     }
     
